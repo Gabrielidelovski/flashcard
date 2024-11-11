6c1be1e1082264c6bd026d8a1eeab173c6fcd82d
Gabrielidelovski <164918814+Gabrielidelovski@users.noreply.github.com>
2024-11-11 14:23:49 -0300

    sei la

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Programação',
-    'O que é ',
-    'O JavaScript é uma linguagem de programação',
+    'O que é Python',
+    'O Python é uma linguagem de programação',
 )
 
 criaCartao(
